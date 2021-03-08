e666acc2d2d2104af86495289359dc5a2b811d02
masa <masatoshi.sboard@gmail.com>
2021-03-08 19:45:42 +0900

    telescope

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -22,7 +22,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '[Space]q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-
+  buf_set_keymap("n", "[Space]f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
     buf_set_keymap("n", "[Space]f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)

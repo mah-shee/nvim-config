c982c48e8513c20ee43ff948a73a80a51a4b00e0
mah-shee <masa@masatoshi.local>
2023-08-16 00:19:38 +0900

    Add keymaps for diagnostic navigation and code actions in LSP setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -90,3 +90,41 @@ nmap <F10> <cmd>call vimspector#StepInto()<cr>")
 api.nvim_set_keymap('n', "Db", ":call vimspector#ToggleBreakpoint()<cr>", {noremap=true, silent=false})
 api.nvim_set_keymap('n', "Dw", ":call vimspector#AddWatch()<cr>", {noremap=true, silent=false})
 api.nvim_set_keymap('n', "De", ":call vimspector#Evaluate()<cr>", {noremap=true, silent=false})
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '[Space]e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '[Space]q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', '[Space]d', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '[Space]h', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '[Space]wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '[Space]wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '[Space]wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '[Space]t', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '[Space]r', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '[Space]a', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'kSpacekn', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '[Space]f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -13,30 +13,6 @@ return {
 				local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 				local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-				buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-				-- Mappings.
-				local opts = { noremap=true, silent=true }
-				buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-				buf_set_keymap('n', '[Space]d', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-				buf_set_keymap('n', '[Space]h', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-				buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-				buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-				buf_set_keymap('n', '[Space]wa', '<cmd>lua vim.lsp.buf.add_workSpace_folder()<CR>', opts)
-				buf_set_keymap('n', '[Space]wr', '<cmd>lua vim.lsp.buf.remove_workSpace_folder()<CR>', opts)
-				buf_set_keymap('n', '[Space]wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workSpace_folders()))<CR>', opts)
-				buf_set_keymap('n', '[Space]t', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-				buf_set_keymap('n', '[Space]r', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-				buf_set_keymap('n', '[Space]n', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-				buf_set_keymap('n', '[Space]e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-				buf_set_keymap('n', '[Space]q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-				buf_set_keymap("n", "[Space]f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-				-- Set some keybinds conditional on server capabilities
-				if client.resolved_capabilities.document_formatting then
-					buf_set_keymap("n", "[Space]f", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
-				elseif client.resolved_capabilities.document_range_formatting then
-					buf_set_keymap("n", "[Space]f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-				end
 			end
 
 			nvim_lsp.rust_analyzer.setup({
@@ -104,9 +80,6 @@ return {
 				},
 			}
 
-			vim.keymap.set('n', '[Space]a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-			vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-			vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 			-- LSP handlers
 			vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 				vim.lsp.diagnostic.on_publish_diagnostics, {

1c9058ebafef3d71da97f414a01c18da5497943c
mah-shee <masa@masatoshi.local>
2023-08-17 22:17:01 +0900

    Update keymap for LSP references to use [Space]n instead of kSpacekn

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -122,7 +122,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', '[Space]t', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '[Space]r', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '[Space]a', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'kSpacekn', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '[Space]n', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '[Space]f', function()
       vim.lsp.buf.format { async = true }
     end, opts)

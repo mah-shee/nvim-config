b5b1d56199c4b33d40ac1593f8b0e4b3b44119a6
mah-shee <masa@masatoshi.local>
2023-09-07 09:08:28 +0900

    Update telescope.nvim to version 0.1.2 with plenary.nvim dependency

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		'nvim-telescope/telescope.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' },
+		dependencies = { 'nvim-lua/plenary.nvim', },
 		tag = '0.1.2',
 		keys = {
 			{ '/', ':Telescope live_grep<CR>' }
